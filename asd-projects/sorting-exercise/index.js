2a937556195d929e05eef161c8a0b15eb0bf60f3
robert couret <rcouret4@gmail.com>
2024-06-26 14:59:16 +0000

    asdfasdf

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -15,15 +15,82 @@ The CSS ids you will work with are:
 
 // TODO 2: Implement bubbleSort
 
+async function bubbleSort (array) {
+    
+
+for (var i = 0; i< array.length-1; i++) {
+    
+    for (var j = array.length-1; j > i+1; j--) {
+        
+     
+    
+    
+if ((array[j].value) < (array[j-1].value))
+    {console.log("trigger")
+        swap(array,j,j-1)
+
+
+updateCounter(bubbleCounter);
+await sleep();
+    }
+}
+
+}
+
+
+}
+
+
+
 
 // TODO 3: Implement quickSort
 
+async function quickSort (array,le,ri) {
+ 
+if ((ri - le) > 0) {
+    var index = partition(array,le,ri)
+}
+if (le < (index - 1))
+    {quickSort(array, le, index - 1)}
+if (index < ri) {
+    quickSort(array, index, ri)
+}
+}
 
 // TODOs 4 & 5: Implement partition
 
+async function partition (array,le,ri) {
+    var pivot = array[Math.floor((ri + le)/2)].value;
+   
+
+while (array[le].value < array[ri].value) {
+  console.log(pivot+" "+array[le].value+" "+array[ri].value)
+
+    while (array[le].value < pivot ) {array[le].value = array[le].value+1
+        console.log(pivot+" "+array[le].value+" "+array[ri].value)
+    }
+    while (array[ri].value > pivot ) {array[ri].value = array[ri].value-1
+        console.log(pivot+" "+array[le].value+" "+array[ri].value)
+    }
+   
+    if (array[le].value< array[ri].value) {
+        swap(array,le,ri)
+        updateCounter(bubbleCounter);
+await sleep();
+    }
+}
+return(le+1)
+}
 
 // TODO 1: Implement swap
 
+function swap (array,i,j) {
+    // forgot what this is called, but it switches the indexes without ever having to create a variable
+    [array[i], array[j]] = [array[j], array[i]]
+    drawSwap(array, i, j)
+        // pass array,i,j to drawSwap
+    
+}
 
 ///////////////////////////////////////////////////////////////////////
 /////////////////////// YOUR WORK GOES ABOVE HERE /////////////////////
